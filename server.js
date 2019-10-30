fb8c25dc199cf3ea3ae68068c46c14d19f5b0aec
Hypetreme <hypetremethewanderer@gmail.com>
2019-10-30 17:32:30 +0200

    m

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+app.use('/',
+    serveStatic(path.join(__dirname, '/dist')))
 
 app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
